8687cfe69c075eb1e9b9c592d27ea3ffaf9c5656
flyinglimao <hi@limaois.me>
2021-08-07 18:26:54 +0800

    Initial commit

---
+++ blog/2021-08-07-varianft-alpha-release-notes.md
@@ -0,0 +1,16 @@
+---
+title: VariaNFT Alpha Release Notes
+author: Kelui
+author_title: Creator of VariaNFT
+author_url: https://github.com/flyinglimao
+author_image_url: https://graph.facebook.com/v11.0/3079946352240797/picture/?height=200&width=200
+tags: [release-notes]
+description: Some info about VariaNFT
+---
+Welcome to this blog. This blog is created with [**Docusaurus 2**](https://docusaurus.io/).
+
+<!--truncate-->
+
+This is my first post on Docusaurus 2.
+
+A whole bunch of exploration to follow.
---
+++ docusaurus.config.js
@@ -0,0 +1,108 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'VariaNFT',
+  tagline: 'Create Your NFTs with SVG and CSV',
+  url: 'https://docs.varianft.studio',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'VariaNFT',
+  projectName: 'docs',
+  trailingSlash: false,
+  themeConfig: {
+    navbar: {
+      logo: {
+        src: 'img/logo.svg'
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/VariaNFT/',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Facebook',
+              href: 'https://www.facebook.com/Varianft-106185488388491',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/4U5BCRHTRj',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/varianft',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/varianft/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} VariaNFT. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/varianft/docs/edit/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/varianft/docs/edit/main/blog',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
