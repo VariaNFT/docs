d252fbe937b42cd323c8b0f048b34e7ddd4c3574
flyinglimao <hi@limaois.me>
2021-08-16 21:16:56 +0800

    feat: update home page

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,32 +4,34 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
+    title: 'Stop Repeat',
+    Svg: require('../../static/img/undraw_Chilling.svg').default,
+    description: (
+      <>
+        VariaNFT lets you mint different NFTs to your targets without editing
+        and uploading bunch of files.
+      </>
+    ),
+  },
+  {
     title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('../../static/img/undraw_Portfolio_update.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        VariaNFT accepts SVG and CSV which can be retrieved from many design
+        software and data source.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Strong Supported',
+    Svg: require('../../static/img/undraw_programme.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        VariaNFT used{' '}
+        <a href="//rarible.com/" target="_blank" referrerPolicy="no-referrer">Rarible</a>{' '}
+        and <a href="//nft.storage/" target="_blank" referrerPolicy="no-referrer">NFT.storage</a>.{' '}
+        The NFTs on chain are secure and the data are delievered via IPFS.
       </>
     ),
   },
